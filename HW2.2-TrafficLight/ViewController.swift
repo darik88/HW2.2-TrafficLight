42508c535e1d71057e962facd64af1625058b2a8
darik88 <mr.darik88@gmail.com>
2020-10-27 17:06:36 +0300

    Initial Commit

---
+++ HW2.2-TrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.2-TrafficLight
+//
+//  Created by Айдар Рахматуллин on 27.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
