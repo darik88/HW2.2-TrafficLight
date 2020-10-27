1466ed383ef18a2c0faa3e71875f61c8650b6bd1
darik88 <mr.darik88@gmail.com>
2020-10-27 18:14:31 +0300

    initial commit

--- HW2.2-TrafficLight/ViewController.swift
+++ HW2.2-TrafficLight/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+        
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var actionButton: UIButton!
+    
+    enum TrafficLight {
+        case red, yellow, green
+    }
+    
+    var currentLight = TrafficLight.red
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+        
+        setLightness(0.3, for: redLightView)
+        setLightness(0.3, for: yellowLightView)
+        setLightness(0.3, for: greenLightView)
+
+        actionButton.setTitle("START", for: .normal)
+        
+        setCornerRadius(for: redLightView)
+        setCornerRadius(for: yellowLightView)
+        setCornerRadius(for: greenLightView)
+    }
+    
+    func setLightness(_ alpha: CGFloat, for view: UIView) {
+        view.alpha = alpha
+    }
+    
+    func setCornerRadius(for view: UIView) {
+        
+        view.layer.cornerRadius = view.frame.width / 2
     }
 
-
+    @IBAction func actionButtonPressed() {
+        actionButton.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            setLightness(0.3, for: greenLightView)
+            setLightness(1, for: redLightView)
+            currentLight = .yellow
+        case .yellow:
+            setLightness(0.3, for: redLightView)
+            setLightness(1, for: yellowLightView)
+            currentLight = .green
+        case .green:
+            setLightness(0.3, for: yellowLightView)
+            setLightness(1, for: greenLightView)
+            currentLight = .red
+        }
+    }
+    
 }
-
